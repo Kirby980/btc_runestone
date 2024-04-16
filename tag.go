688cb10016dbef45a1561a13f43aff2f085de95e
Kirby980 <hyz17779903860@gmail.com>
2024-04-16 18:09:58 +0800

    add tag

---
+++ tag.go
@@ -0,0 +1,25 @@
+package btc_runestone
+
+const (
+	TagBody         = 0
+	TagFlags        = 2
+	TagRune         = 4
+	TagPremine      = 6
+	TagCap          = 8
+	TagAmount       = 10
+	TagHeightStart  = 12
+	TagHeightEnd    = 14
+	TagOffsetStart  = 16
+	TagOffsetEnd    = 18
+	TagMint         = 20
+	TagPointer      = 22
+	TagCenotaph     = 126
+	TagDivisibility = 1
+	TagSpacers      = 3
+	TagSymbol       = 5
+	TagNop          = 127
+	FlagEtching     = 0
+	FlagTerms       = 1
+	FlagCenotaph    = 127
+	FlagTurbo       = 2
+)
